017f776dbc574ec6ca769261d54fe02bcb1058b8
Lucas <18538046+TeaWithLucas@users.noreply.github.com>
2017-10-16 14:58:58 +0100

    Structure shift

--- player.py
+++
@@ -1,8 +0,0 @@
-""" THIS IS THE PLAYER CLASS THAT HOLDS A TEMPLATE FOR PLAYER OBJECTS """
-
-class Player():
-    def __init__(self):
-        self.name = ''
-        self.health = 100
-        self.inv = []
-        self.level = 1
